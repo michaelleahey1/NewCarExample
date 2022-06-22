69d7b3b3009fe64bac468cfa85a658607e93e7df
Michael Leahey <michael.leahey1843@outlook.com>
2022-06-22 11:38:49 -0400

    update

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,7 +1,64 @@
 package org.example;
 
-public class Main {
+public class Car {
+    //objects
+
+    private String model;
+    private String make;
+    private double price;
+    private int year;
+    private String color;
+
+    //constructors
+
+    public Car(String model, String make, double price, int year, String color) {
+        this.model = model;
+        this.make = make;
+        this.price = price;
+        this.year = year;
+        this.color = color;
+    }
+
+
+    // getters/setters
+
+
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getMake() {
+        return make;
+    }
+    public void setMake(String make) {
+        this.make = make;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car c = new Car("Ford", "mustang",70000,1968,"yellow");
+        System.out.println(" Model " + c.getModel() + " Make: " + c.getMake() + " Price " +c.getPrice() +  " Year " + c.getYear() + " Color" + c.getColor() );
+        Car c1 = new Car("Chevy", "Corvette", 80000,1963, "red");
+        System.out.println(" Model " + c1.getModel() + " Make " + c1.getMake() + " Price " + c1.getPrice() +  " Year " + c1.getYear() + " Color " + c1.getColor() );
+        Car c2 = new Car("Pontiac", "GTO,", 26000, 1970, "green");
+        System.out.println(" Model " + c2.getModel() + " Make " + c2.getMake() + " Price "+c2.getPrice() +  " year " + c2.getYear() + " Color " + c2.getColor());
     }
 }
